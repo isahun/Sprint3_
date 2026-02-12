d9bc9c46aa923a9f556257098c6d5eebb33891ce
Irene Virgínia <isahun7@gmail.com>
2026-02-12 16:44:13 +0100

    feat: initialize app state and DOM references

--- api-consumer-app/src/main.js
+++ api-consumer-app/src/main.js
@@ -1,4 +1,19 @@
 "use strict"
 
 import "./style.css"
-import axios from "axios";
+
+const API_URL = "https://jsonplaceholder.typicode.com/posts";
+let currentPage = 1;
+const itemsPerPage = 10;
+
+
+//DOM elements refs
+const apiSelector = document.getElementById("fetch-method");
+const searchInput = document.getElementById("search-input");
+const fetchButton = document.getElementById("fetch-btn");
+const loadingElement = document.getElementById("loading");
+const errorElement = document.getElementById("error");
+const resultsContainer = document.getElementById("results");
+const paginationContainer = document.getElementById("pagination");
+
+
